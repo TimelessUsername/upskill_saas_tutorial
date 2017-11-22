a1e1a2b9c859dfb3d67a8f2f55269a0a8eb10cb8
Santtu Parviainen <parviainen@windowslive.com>
2017-11-22 17:34:40 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+    default to: 'throwaway7321@outlook.com'
+   
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+    mail(form: email, subject: 'Contact Form Message')
+    end
+end
