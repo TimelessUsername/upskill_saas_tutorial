480f672c5c58d9d4bf505416fc460ad32cce9c19
Santtu Parviainen <parviainen@windowslive.com>
2017-11-23 18:06:31 +0000

    Added additional form validation(not included in the course?)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,15 @@
-class Contact < ActiveRecord::Base
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :comments, presence: true
-    
+class EmailValidator < ActiveModel::Validator
+    def validate(contact_params)
+        unless contact_params[:email].include?("@")
+            contact_params.errors[:base] << "Email must contain @ symbol"
+        end
+    end
 end
+
+class Contact < ActiveRecord::Base
+    validates :name, :comments, :email, presence: true
+    validates :name, :comments, :email, length: { minimum: 2, message: "2 Letters minimum" }
+    validates :name, :comments, :email, length: { maximum: 254, message: "254 Letters maximum" }
+    validates_with EmailValidator
+end
+
