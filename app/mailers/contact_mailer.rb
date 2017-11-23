ce5232ce27d3585ae21308d1c3dc3aff03cedfd3
Santtu Parviainen <parviainen@windowslive.com>
2017-11-23 15:47:53 +0000

    bugfix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ActionMailer::Base
     default to: 'throwaway7321@outlook.com'
-   
     def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
-        
-    mail(form: email, subject: 'Contact Form Message')
+        mail(form: email, subject: 'Contact Form Message')
     end
 end
